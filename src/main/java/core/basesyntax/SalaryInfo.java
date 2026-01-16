ff97a58a906e767a70b1afa3c579c2ebf40b85b6
Kaminskyi <kaminskyiwladyslaw@gmail.com>
2026-01-16 14:53:19 +0100

    hw

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,13 +4,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    private static final int DAY_DATE = 0;
     private static final int NAME_EMPLOYER = 1;
     private static final int HOURS_DAY = 2;
     private static final int SALARY_HOUR = 3;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate from = LocalDate.parse(dateFrom, formatter);
         LocalDate to = LocalDate.parse(dateTo, formatter);
         StringBuilder information = new StringBuilder();
@@ -26,10 +25,11 @@ public class SalaryInfo {
                 if (name.equals(info[NAME_EMPLOYER])
                         && !dateDay.isBefore(from)
                         && !dateDay.isAfter(to)) {
-                    salary += Integer.parseInt(info[SALARY_HOUR]) * Integer.parseInt(info[HOURS_DAY]);
+                    salary += Integer.parseInt(info[SALARY_HOUR])
+                            * Integer.parseInt(info[HOURS_DAY]);
                 }
             }
-            information.append("\r\n")
+            information.append(System.lineSeparator())
                     .append(name)
                     .append(" - ")
                     .append(salary);
