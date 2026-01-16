04fe85f5982bc07e0a319a17945f16068984ae49
Kaminskyi <kaminskyiwladyslaw@gmail.com>
2026-01-16 14:45:04 +0100

    hw

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final int DAY_DATE = 0;
+    private static final int NAME_EMPLOYER = 1;
+    private static final int HOURS_DAY = 2;
+    private static final int SALARY_HOUR = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
+        StringBuilder information = new StringBuilder();
+        information.append("Report for period ")
+                .append(dateFrom)
+                .append(" - ")
+                .append(dateTo);
+        for (String name : names) {
+            int salary = 0;
+            for (int i = 0; i < data.length; i++) {
+                String[] info = data[i].split(" ");
+                LocalDate dateDay = LocalDate.parse(info[0], formatter);
+                if (name.equals(info[NAME_EMPLOYER])
+                        && !dateDay.isBefore(from)
+                        && !dateDay.isAfter(to)) {
+                    salary += Integer.parseInt(info[SALARY_HOUR]) * Integer.parseInt(info[HOURS_DAY]);
+                }
+            }
+            information.append("\r\n")
+                    .append(name)
+                    .append(" - ")
+                    .append(salary);
+        }
+        return information.toString();
     }
 }
